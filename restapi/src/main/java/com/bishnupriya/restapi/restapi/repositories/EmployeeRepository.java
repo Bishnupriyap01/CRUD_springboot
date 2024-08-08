ddd4274dfa099caf41776eb13ccf4de5c79c5a8e
Bishnupriya Pradhan <86412967+Bishnupriyap01@users.noreply.github.com>
2024-08-08 15:28:50 +0530

    Update EmployeeRepository.java

--- restapi/src/main/java/com/bishnupriya/restapi/restapi/repositories/EmployeeRepository.java
+++ restapi/src/main/java/com/bishnupriya/restapi/restapi/repositories/EmployeeRepository.java
@@ -3,8 +3,12 @@ package com.bishnupriya.restapi.restapi.repositories;
 import com.bishnupriya.restapi.restapi.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+  @Query("SELECT e FROM EmployeeEntity e WHERE LOWER(e.name) LIKE LOWER(CONCAT('%', :name, '%'))")
+    List<EmployeeEntity> findByName_(@Param("name") String name);
 
 }
